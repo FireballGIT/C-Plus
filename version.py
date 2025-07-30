c3c676bb14689388efbf37321cf50ecb28501210
FireballGIT <gafutb@outlook.com>
2025-07-30 14:35:05 -0500

    Create version.py

---
+++ version.py
@@ -0,0 +1,17 @@
+class Version:
+    def __init__(self):
+        self.version = ""
+
+    def update_version(self, new_version: int, state: int) -> str:
+        match state:
+            case 1:
+                state_str = "Alpha"
+            case 2:
+                state_str = "Beta"
+            case 3:
+                state_str = "Stable"
+            case _:
+                state_str = "Unknown"
+
+        self.version = f"{state_str} {new_version}"
+        return self.version
